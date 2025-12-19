9adb7131a62d2f95158b8989ea701d7cc001e166
malavikamb2k1-create <malavikamb2k1@gmail.com>
2025-12-19 08:33:53 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('colleges/', views.college_list, name='college_list'),
+    path('aptitude-test/', views.aptitude_test, name='aptitude_test'),
+    path('result/', views.result, name='result'),
+]
